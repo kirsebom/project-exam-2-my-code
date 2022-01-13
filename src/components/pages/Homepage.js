b99dba1a19ae45a9ca182f8d341e149e0bfd25c3
kirsebom <olemartinkirsebom@gmail.com>
2022-01-13 12:40:33 +0100

    13.01.2022 second commit

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -8,7 +8,10 @@ const Homepage = () => {
 			<div className={styles.container}>
 				<p className={styles.text}>Do you want to visit Bergen?</p>
 				<p className={styles.text}>Perfect!</p>
-				<button className={styles.button}>Book a place to stay</button>
+				<div className={styles.button_container}>
+					<button className={styles.button}>Book a place to stay</button>
+					<div className={styles.button_background}></div>
+				</div>
 			</div>
 		</div>
 	);
